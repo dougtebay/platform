fcf5d6858d7b3b5ebc7360e49ef068251fbd4f22
dougtebay <dougtebay@gmail.com>
2016-11-13 16:46:57 -0500

    Set up for use with Grunt, Browserify, and Babel

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function (grunt) {
+
+   grunt.initConfig({
+      browserify: {
+         dist: {
+            options: {
+               transform: [
+                  ["babelify", {
+                     loose: "all"
+                  }]
+               ]
+            },
+            files: {
+               "./app/app.js": ["./src/main.js"]
+            }
+         }
+      },
+      watch: {
+         scripts: {
+            files: ["./src/*.js"],
+            tasks: ["browserify"]
+         }
+      }
+   });
+
+   grunt.loadNpmTasks("grunt-browserify");
+   grunt.loadNpmTasks("grunt-contrib-watch");
+
+   grunt.registerTask("default", ["watch"]);
+   grunt.registerTask("build", ["browserify"]);
+
+};
